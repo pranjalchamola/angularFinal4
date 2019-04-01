d035b6fdbd9cb5d029655aa7e76565bbd163b7d1
pranjalchamola <pranjalchamola@gmail.com>
2019-04-01 18:47:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { HeroImageComponent } from './hero-image/hero-image.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainBodyComponent,
+    HeroImageComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    SecondPageComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
