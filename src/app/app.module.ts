c3da4c58bd191fd1113f6c805550c65be2f6bbc3
Sadiya <khansadiya397@gmail.com>
2019-04-03 11:01:25 +0530

    changes in login and register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from "@angular/fire";
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,6 +14,18 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { RouterModule } from '@angular/router';
+import { AuthService } from './auth.service';
+const authRouting: ModuleWithProviders = RouterModule.forChild([
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+]);
 
 @NgModule({
   declarations: [
@@ -22,13 +37,21 @@ import { FavoriteComponent } from './favorite/favorite.component';
     LoginComponent,
     RegisterComponent,
     SecondPageComponent,
-    FavoriteComponent
+    FavoriteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    authRouting,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AuthService } from '../auth.service'
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private authService:AuthService,private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          firstName: ['', [Validators.required,Validators.pattern('^[a-zA-Z0-9]+$')]],
+          lastName: ['',[Validators.required,Validators.pattern('^[a-zA-Z0-9]+$')]],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6)]]
+      });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+   
+  onSubmit() {
+    this.authService.signup(this.registerForm.value.email, this.registerForm.value.password);
+    this.registerForm.value.email = this.registerForm.value.password = '';
+}
 }
